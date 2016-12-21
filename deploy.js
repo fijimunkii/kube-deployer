54e3bfd849a1af9337bbe9c3928ddd4860a315cd
Harrison Powers <harrisonpowers@gmail.com>
2016-12-21 14:43:27 -0500

    Set deployment status

--- deploy.js
+++ deploy.js
@@ -7,6 +7,7 @@ var request = Promise.promisifyAll(require('request'),{multiArgs:true});
 var execAsync = Promise.promisify(require('child_process').exec);
 
 module.exports = (req, res) => {
+  var apphost;
   getDeployment({
     username: req.body.repository.owner.login,
     reponame: req.body.repository.name,
@@ -15,7 +16,7 @@ module.exports = (req, res) => {
     res.sendStatus(200);
     if (!(new RegExp(env.get('branch')||'.*')).test(deployment.ref))
       throw 'Invalid branch';
-    var apphost = ( env.get('production') ?
+    apphost = ( env.get('production') ?
       deployment.environment === 'production' && 'app' || 'stg' :
       deployment.ref ) + '.' + env.get('APPHOST');
     var cmd = [
@@ -31,9 +32,16 @@ module.exports = (req, res) => {
     ].join(' ');
     console.log(cmd);
     return execAsync(cmd)
-      .then(res => {
-        console.log(res);
-      });
+      .then(console.log)
+      .then(() => sendMessage('Deployed https://'+apphost))
+      .then(() => sendStatus({
+        username: req.body.repository.owner.login,
+        reponame: req.body.repository.name,
+        id: deployment.id,
+        state: 'success',
+        description: 'Deployed: ' + targetUrl + ' ' + reason,
+        target_url: apphost
+      }));
   })
   .catch(err => {
     if (err === 'Invalid branch') return;
@@ -46,14 +54,14 @@ module.exports = (req, res) => {
     console.log(err && err.stack || err);
     res.status(500).end(String(err));
     return Promise.all([
-      sendMessage('Deploy FAILED ' + targetUrl + ' ' + reason),
+      sendMessage('Deploy FAILED ' + apphost + ' ' + reason),
       sendStatus({
         username: req.query.username,
         reponame: req.query.reponame,
-        rev: rev,
+        id: req.body.deployment.id,
         state: 'failure',
-        description: 'Deploy FAILED: ' + targetUrl + ' ' + reason,
-        target_url: targetUrl
+        description: 'Deploy FAILED: ' + apphost + ' ' + reason,
+        target_url: apphost
       })
     ]);
   });
